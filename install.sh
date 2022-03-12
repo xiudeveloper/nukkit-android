be1c61badefb8dbb0b3bb0a1561c03bc9288df44
xiudeveloper <65165516+xiudeveloper@users.noreply.github.com>
2022-03-12 10:59:04 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo Nukkit Android Installer
+echo Version: 1.0
+echo Author: github.com/xiudeveloper
+echo -------------------------------
+echo STARTING INSTALLATION
+echo -------------------------------
+sleep 2
+echo Test
