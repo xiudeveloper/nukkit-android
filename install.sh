5412a7cc57faa963f5c514dee63b44cb4729ef5b
xiudeveloper <65165516+xiudeveloper@users.noreply.github.com>
2022-03-12 11:08:15 +0000

    yes

--- install.sh
+++ install.sh
@@ -5,4 +5,28 @@ echo -------------------------------
 echo STARTING INSTALLATION
 echo -------------------------------
 sleep 2
-echo Test
+echo Installing Java...
+apt update
+apt upgrade -y
+apt install default-jdk -y
+apt update
+apt install software-properties-common -y
+apt update
+clear
+echo If you are prompted anything here, press ENTER.
+add-apt-repository ppa:webupd8team/java -y
+clear
+echo Java has been installed, Nukkit will now build. This may take a while.
+sleep 2
+apt install git -y
+apt install curl -y 
+apt install wget -y
+git clone https://github.com/CloudburstMC/Nukkit
+cd Nukkit
+git submodule update --init
+chmod +x mvnw
+./mvnw clean package
+clear
+cd target
+echo Installing done. You can run Nukkit with the following command:
+echo java -jar nukkit-1.0-SNAPSHOT.jar
